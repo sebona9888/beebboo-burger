fdbafc79594545d35342f6303a83ebe310f4e680
Sebona Haile <sebonahaile@Gmail.com>
2026-05-01 11:50:53 +0300

    Moved navbar to left and fixed mobile text size

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// Path kana './useCart' tti jijjiiri
 import { useCart } from '../../context/useCart';
 import './Navbar.css';
 
 const Navbar = () => {
     const { cartCount } = useCart();
 
+    // Linkiiwwan akka hin banneef bifa qulqulluun sirreessuuf
+    const linkStyle = {
+        textDecoration: 'none',
+        color: 'inherit', // Halluu isaa dizaayinii CSS irraa fudhata
+        display: 'inline-block'
+    };
+
     return (
         <nav className="navbar">
-            <Link to="/" className="logo">
-                Beebboo <span>Burger</span>
+            <Link to="/" className="logo" style={linkStyle}>
+                Beebboo <span style={{ color: '#ff9d00' }}>Burger</span>
             </Link>
 
             <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/payment">Payment</Link></li>
-                <li><Link to="/admin">Admin</Link></li>
+                <li><Link to="/" style={linkStyle}>Home</Link></li>
+                <li><Link to="/menu" style={linkStyle}>Menu</Link></li>
+                <li><Link to="/about" style={linkStyle}>About</Link></li>
+                <li><Link to="/contact" style={linkStyle}>Contact</Link></li>
+                <li><Link to="/payment" style={linkStyle}>Payment</Link></li>
+                <li><Link to="/admin" style={linkStyle}>Admin</Link></li>
             </ul>
 
             <div className="nav-icons">
-                <Link to="/cart" className="cart-btn">
+                <Link to="/cart" className="cart-btn" style={{ ...linkStyle, color: '#000' }}>
                     🛒 Cart ({cartCount})
                 </Link>
             </div>
