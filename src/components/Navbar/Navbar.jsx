d9f1b540ef3e9a503adfd872e3229c960fb5cba1
Sebona Haile <sebonahaile@Gmail.com>
2026-05-03 21:46:26 +0300

    Responsive navbar with sidebar and 3D logo fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from '../../context/useCart';
 import './Navbar.css';
 
 const Navbar = () => {
     const { cartCount } = useCart();
+    const [isOpen, setIsOpen] = useState(false); // Meenuu too'achuuf
+
+    const toggleMenu = () => setIsOpen(!isOpen);
 
     const linkStyle = {
         textDecoration: 'none',
         display: 'inline-block'
     };
 
-    // Dizaayinii 3D (Osoo halluu hin jijjiirin shadow qofa itti daballe)
     const logo3DStyle = {
         ...linkStyle,
         color: '#fff',
         fontWeight: 'bold',
         fontSize: '24px',
-        textShadow: `
-            1px 1px 0px #ccc, 
-            2px 2px 0px #bbb, 
-            3px 3px 0px #aaa, 
-            4px 4px 5px rgba(0,0,0,0.5)`
+        textShadow: `1px 1px 0px #ccc, 2px 2px 0px #bbb, 3px 3px 0px #aaa, 4px 4px 5px rgba(0,0,0,0.5)`
     };
 
     const burger3DStyle = {
         color: '#ff9d00',
-        textShadow: `
-            1px 1px 0px #d48400, 
-            2px 2px 0px #b06d00, 
-            3px 3px 0px #8a5500, 
-            4px 4px 5px rgba(0,0,0,0.5)`
+        textShadow: `1px 1px 0px #d48400, 2px 2px 0px #b06d00, 3px 3px 0px #8a5500, 4px 4px 5px rgba(0,0,0,0.5)`
     };
 
     return (
@@ -39,13 +33,21 @@ const Navbar = () => {
                 Beebboo <span style={burger3DStyle}>Burger</span>
             </Link>
 
-            <ul className="nav-links">
-                <li><Link to="/" style={{ ...linkStyle, color: '#fff' }}>Home</Link></li>
-                <li><Link to="/menu" style={{ ...linkStyle, color: '#fff' }}>Menu</Link></li>
-                <li><Link to="/about" style={{ ...linkStyle, color: '#fff' }}>About</Link></li>
-                <li><Link to="/contact" style={{ ...linkStyle, color: '#fff' }}>Contact</Link></li>
-                <li><Link to="/payment" style={{ ...linkStyle, color: '#fff' }}>Payment</Link></li>
-                <li><Link to="/admin" style={{ ...linkStyle, color: '#fff' }}>Admin</Link></li>
+            {/* Sarara Sadii (Hamburger) - Mobile irratti qofa mul'ata */}
+            <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+
+            {/* Nav Links - Mobile irratti sidebar ta'a */}
+            <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <li><Link to="/" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>Home</Link></li>
+                <li><Link to="/menu" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>Menu</Link></li>
+                <li><Link to="/about" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>About</Link></li>
+                <li><Link to="/contact" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>Contact</Link></li>
+                <li><Link to="/payment" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>Payment</Link></li>
+                <li><Link to="/admin" style={{ ...linkStyle, color: '#fff' }} onClick={() => setIsOpen(false)}>Admin</Link></li>
             </ul>
 
             <div className="nav-icons">
